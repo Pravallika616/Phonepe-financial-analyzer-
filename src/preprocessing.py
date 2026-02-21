36102d901a2764aa620aba7548aee3af04041911
Pravallika Girishetti <girishettipravallika@gmail.com>
2026-02-21 13:44:20 +0530

    Added notebook and done the anlysis

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,10 +1,14 @@
+# %%
 
 import pandas as pd
 
 def main():
     df = pd.read_csv("data/sample_transaction.csv",sep="\t" )
     print(df.head())
+    #cchecking datatypes
     print(df.dtypes)
+
+
 
 if __name__ == "__main__":
     main()
